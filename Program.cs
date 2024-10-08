138ef3140ca9fb78fde08d60b7277d656627bccd
Danil Stukalov <s107@mokeevskaya.ru>
2024-10-08 20:52:26 +0300

    Add new file

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿double a;
+double b;
+double total;
+char oper;
+
+Console.WriteLine("Введите первое число:");
+a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Выберите действие (+,-,*,/):");
+oper = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+b = Convert.ToDouble(Console.ReadLine());
+
+if (oper == '+')
+{
+    total = a + b;
+    Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
+}
+
+else if (oper == '-')
+{
+    total = a - b;
+    Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
+}
+
+else if (oper == '*')
+{
+    total = a * b;
+    Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
+}
+
+else if (oper == '/')
+{
+    total = a / b;
+    Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
+}
+else
+{
+    Console.WriteLine("Неизвестный оператор.");
+}
