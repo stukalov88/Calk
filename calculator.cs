6667333d0a6828a04f7277b2f4d6bbff173b1003
Danil Stukalov <s107@mokeevskaya.ru>
2024-10-11 23:16:12 +0300

    error

--- calculator.cs
+++ calculator.cs
@@ -3,41 +3,64 @@ double b;
 double total;
 char oper;
 
-/* простой калькулятор (+ - * \) */
-
-Console.WriteLine("Введите первое число:");
-a = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Выберите действие (+,-,*,/):");
-oper = Convert.ToChar(Console.ReadLine());
-
-Console.WriteLine("Введите второе число:");
-b = Convert.ToDouble(Console.ReadLine());
-
-if (oper == '+')
+while (true) // Бесконечный цикл, чтобы программа продолжалась
 {
-    total = a + b;
-    Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
-}
+    try
+    {
+        Console.WriteLine("Введите первое число:");
+        a = Convert.ToDouble(Console.ReadLine());
 
-else if (oper == '-')
-{
-    total = a - b;
-    Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
-}
+        Console.WriteLine("Введите оператор (+, -, *, /):");
+        oper = Convert.ToChar(Console.ReadLine());
 
-else if (oper == '*')
-{
-    total = a * b;
-    Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
-}
+        Console.WriteLine("Введите второе число:");
+        b = Convert.ToDouble(Console.ReadLine());
 
-else if (oper == '/')
-{
-    total = a / b;
-    Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
-}
-else
-{
-    Console.WriteLine("Неизвестный оператор.");
+        if (oper == '+')
+        {
+            total = a + b;
+            Console.WriteLine("Сумма " + a + " и " + b + " равна " + total + ".");
+        }
+        else if (oper == '-')
+        {
+            total = a - b;
+            Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
+        }
+        else if (oper == '*')
+        {
+            total = a * b;
+            Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
+        }
+        else if (oper == '/')
+        {
+            if (b == 0) // Проверка деления на ноль
+            {
+                Console.WriteLine("Ошибка: деление на ноль невозможно.");
+            }
+            else
+            {
+                total = a / b;
+                Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Неизвестный оператор.");
+        }
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Ошибка: пожалуйста, введите правильное число.");
+    }
+    catch (Exception ex) // Общая обработка исключений
+    {
+        Console.WriteLine("Произошла ошибка: " + ex.Message);
+    }
+
+    Console.WriteLine("Хотите продолжить? (y/n):");
+    char continueInput = Convert.ToChar(Console.ReadLine());
+    if (continueInput != 'y' && continueInput != 'Y')
+    {
+        break; // Выход из цикла
+    }
 }
