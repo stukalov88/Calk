680cc385e3d1ea13b3c0c80172e5a0ee9ec789d3
Danil Stukalov <s107@mokeevskaya.ru>
2024-10-11 22:57:34 +0300

    delnull

--- calculator.cs
+++ calculator.cs
@@ -3,12 +3,10 @@ double b;
 double total;
 char oper;
 
-/* простой калькулятор (+ - * \) */
-
 Console.WriteLine("Введите первое число:");
 a = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Выберите действие (+,-,*,/):");
+Console.WriteLine("Введите оператор:");
 oper = Convert.ToChar(Console.ReadLine());
 
 Console.WriteLine("Введите второе число:");
@@ -17,25 +15,29 @@ b = Convert.ToDouble(Console.ReadLine());
 if (oper == '+')
 {
     total = a + b;
-    Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
+    Console.WriteLine("Сумма " + a + " и " + b + " равна " + total + ".");
 }
-
 else if (oper == '-')
 {
     total = a - b;
     Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
 }
-
 else if (oper == '*')
 {
     total = a * b;
     Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
 }
-
 else if (oper == '/')
 {
-    total = a / b;
-    Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
+    if (b == 0)
+    {
+        Console.WriteLine("Ошибка: деление на ноль невозможно.");
+    }
+    else
+    {
+        total = a / b;
+        Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
+    }
 }
 else
 {
